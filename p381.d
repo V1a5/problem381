86333196d5307d9ad18da449fc31516189ea1d6e
V1a5 <vlasmamokiak@gmail.com>
2023-12-08 05:53:36 +0700

    problem381

---
+++ p381.d
@@ -0,0 +1,39 @@
+import std.stdio;
+import std.container;
+import std.typecons;
+import std.array;
+import std.algorithm;
+import std.conv;
+
+void main() {
+    string path = "/home/vlas/probem381/data";
+    auto data = parse_input(path);
+    writeln(" ", data.length);
+    write(" ");
+    foreach (point; data) {
+        write(point[0], " ");
+        write(point[1], " ");
+    }
+    writeln();
+}
+
+Array!(Tuple!(int, int)) parse_input(string path) {
+    File file = File(path, "r");
+    int ncase;
+    file.readf!"%d\n"(ncase);
+    auto data = Array!(Tuple!(int, int))();
+    auto parts = file.readln.split.map!(to!int).array;
+
+    for (int i = 0; i < parts.length; i += 2) {
+        Tuple!(int, int) point;
+        point[0] = parts[i];
+        if (i + 1 < parts.length) {
+            point[1] = parts[i + 1];
+        } else {
+            point[1] = 0; // Atasi elemen terakhir yang mungkin tidak ada pasangannya
+        }
+        data ~= point;
+    }
+
+    return data;
+}
